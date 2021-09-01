bc0add61868dc499d3773a979bb0f4d2e2fdb5cf
l.murillo.baltodano <l.murillo.baltodano@accenture.com>
2021-09-01 04:52:48 -0600

    add rituals import to scene

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default () => {
   return (
     <Components.L0.GlobalWrapper title='Home'>
       <Components.L1.Canvas3D
-        scenes={['Default']}
+        scenes={['Default', 'Rituals']}
         id='MainScene'
       />
     </Components.L0.GlobalWrapper>
